bf52dc08aec2ed0933d0b8a100b4750bf6276f98
pedrommaiaa <pedroceduca@gmail.com>
2021-09-01 09:58:19 -0400

    policy evaluation

--- gridword/grid.py
+++ gridword/grid.py
@@ -1,79 +1,104 @@
 import numpy as np
+from gym.envs.toy_text import discrete
 
-GRID_ROWS = 5
-GRID_COLS = 5
-START = (0, 0)
-WIN_STATE = (4, 4)
- 
 
-class gridWorld:    
+class gridWorld(discrete.DiscreteEnv):
+    """
+    Grid World environment from Sutton's Reinforcement Learning book chapter 4.
+    You are an agent on an MxN grid and your goal is to reach the terminal
+    state at the bottom right corner.
 
-    def __init__(self, state=START):
-        self.grid = np.zeros([GRID_ROWS, GRID_COLS])
-        self.nStates = GRID_ROWS * GRID_COLS
-        
-        self.position = state
-        self.done = False
+    For example, a 5x5 grid looks as follows:
 
-        self.reward = 0
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 X 0 0 
+    0 0 0 0 0
+    0 0 0 0 T
 
-        # [left, right, up, down]    
+    x is your position and T are the two terminal states.
+
+    You can take actions in each direction [0=up, 1=Right, 2=Down, 3=Left].
+    Actions going off the edge leave you in your current state.
+    You receive a reward of -1 at eachs tep until you reach a terminal state.
+    """
+    
+    def __init__(self, shape=[5, 5]):
+
+        self.shape = shape
+
+        nS = np.prod(shape)
         self.actions = [0, 1, 2, 3]
-        self.nActions = len(self.actions)
-         
+        nA = len(self.actions)
 
-    # take action
-    def step(self, action):
-        
-        if action not in self.actions:
-            raise ValueError("This is not a valid action.")
+        MAX_Y = shape[0]
+        MAX_X = shape[1]
 
-        # Left
-        if action == 0:
-            nextPos = (self.position[0], self.position[1]-1)
+        P = {}
+        grid = np.arange(nS).reshape(shape)
+        it = np.nditer(grid, flags=['multi_index'])
 
-        # Right
-        if action == 1:
-            nextPos = (self.position[0], self.position[1]+1)
-        
-        # Up
-        if action == 2:
-            nextPos = (self.position[0]-1, self.position[1])
-        
-        # Down
-        if action == 3:
-            nextPos = (self.position[0]+1, self.position[1])
+        while not it.finished:
+            s = it.iterindex
+            y, x = it.multi_index
 
+            # P[s][a] = (prob, next_state, reward, is_done)
+            P[s] = {a : [] for a in range(nA)}
 
-        if (nextPos[0] >= 0) and (nextPos[0] <= (GRID_ROWS - 1)):
-            if (nextPos[1] >= 0) and (nextPos[1] <= (GRID_COLS - 1)):
-                if nextPos != WIN_STATE:
-                    self.position = nextPos
+            is_done = lambda s: s == (nS - 1)
+            reward = 0.0 if is_done(s) else -1.0
 
+            # We're stuck in a terminal state
+            if is_done(s):
+                P[s][self.actions[0]] = [(1.0, s, reward, True)]
+                P[s][self.actions[1]] = [(1.0, s, reward, True)]
+                P[s][self.actions[2]] = [(1.0, s, reward, True)]
+                P[s][self.actions[3]] = [(1.0, s, reward, True)]
+            # Not a terminal state
+            else:
+                ns_up = s if y == 0 else s - MAX_X
+                ns_right = s if x == (MAX_X - 1) else s + 1
+                ns_down = s if y == (MAX_Y - 1) else s + MAX_X
+                ns_left = s if x == 0 else s - 1
+                P[s][self.actions[0]] = [(1.0, ns_up, reward, is_done(ns_up))]
+                P[s][self.actions[1]] = [(1.0, ns_right, reward, is_done(ns_right))]
+                P[s][self.actions[2]] = [(1.0, ns_down, reward, is_done(ns_down))]
+                P[s][self.actions[3]] = [(1.0, ns_left, reward, is_done(ns_left))]
 
-        if self.position == WIN_STATE:
-            self.done = True
+            it.iternext()
 
-        return self.position, self.reward, self.done
+        # Initial state distribution is uniform
+        isd = np.ones(nS) / nS
 
+        # We expose the model of the environment for educational purposes
+        # This should not be used in any model-free learning algorithm
+        self.P = P
 
-    def reset(self):
-        self.position = (0, 0)
-        self.done = False
-
-
-    def randomAction(self):
-        return np.random.choice(self.actions)
-
+        super(gridWorld, self).__init__(nS, nA, P, isd)
 
     def display(self):
-        for i in range(GRID_ROWS):
-            for j in range(GRID_COLS):
-                if (i, j) == self.position:
-                    print('X', end=' ')
-                elif (i, j) == (GRID_ROWS-1, GRID_COLS-1):
-                    print(1, end=' ')
-                else:
-                    print(0, end=' ')
-            print()
+        """ Display the current grid layout """ 
+        grid = np.arange(self.nS).reshape(self.shape)
+        it = np.nditer(grid, flags=['multi_index'])
+        while not it.finished:
+            s = it.iterindex
+            y, x = it.multi_index
 
+            if self.s == s:
+                print('X', end=' ')
+            elif s == self.nS - 1:
+                print('T', end=' ')
+            else:
+                print('0', end=' ')
+
+            if x == self.shape[1] - 1:
+                print()
+
+            it.iternext()
+
+
+if __name__ == "__main__":
+
+    env = gridWorld()
+
+    env.display()
--- gridword/policy-evaluation.py
+++ gridword/policy-evaluation.py
@@ -1,35 +1,56 @@
 import numpy as np
-from grid import Gridworld
+from grid import gridWorld
 
 # Problem: Evaluate a given policy pi
 # Solution: Iterative application of Bellman expectation backup
 
-def policy_evaluation(policy, env):
 
+def policy_evaluation(policy, env, discount_factor=1.0, theta=0.00001):
     """
     Evaluate a policy given an environment anda full description of the 
     environment's dynamics.
 
     Args:
         policy: [S, A] shaped matrix representing the policy.
-        env: Grid World.
-             env.nStates is the number of states in the environment.
-             env.nActions is the number of actions in the environment.
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+             env.nS is the number of states in the environment.
+             env.nA is the number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor
+
+    Returns:
+        Vector of legnth env.nS representing the value function.
     """
 
     # Start with a all 0 value function
-    V = np.zeros(env.nStates)
+    V = np.zeros(env.nS)
     while True:
-        # Implement!
-        break
+        delta = 0
+        # for each state perform a full backup
+        for state in range(env.nS):
+            v = 0
+            # Look at each possiple next action
+            for action, action_prob in enumerate(policy[state]):
+                # For each action, look at the possible next state
+                for prob, next_state, reward, done in env.P[state][action]:
+                    # Calculate the expected value. Ref: Sutton book eq. 4.6.
+                    v += action_prob * prob * (reward + discount_factor * V[next_state])
+            # How much our value function changed (across any states)
+            delta = max(delta, np.abs(v - V[state]))
+            V[state] = v
+        if delta < theta:
+            break
     return np.array(V)
 
 
 
 if __name__ == "__main__":
 
-    env = Gridworld()
+    env = gridWorld()
+    env.display()
     
-    random_policy = np.ones([env.nStates, env.nActions]) / env.nActions
+    random_policy = np.ones([env.nS, env.nA]) / env.nA
     
     v = policy_evaluation(random_policy, env)
+
+    print(f"Value Function:\n {v.reshape(env.shape)}")
