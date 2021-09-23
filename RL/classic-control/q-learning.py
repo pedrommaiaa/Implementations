5bd608345e02b1170a63a8a13d45445afd3196f7
pedrommaiaa <pedroceduca@gmail.com>
2021-09-23 08:16:02 -0400

    q-learning

--- RL/classic-control/q-learning.py
+++ RL/classic-control/q-learning.py
@@ -24,25 +24,33 @@ Solved Requirements:
     trials.
 """
 
-"""
-Q-Learning algorithm: Off-policy TD control. Finds the optimal greedy policy
-while following an epsilon-greedy policy.
+def QCartPoleSolver(buckets=(1, 1, 6, 12,), n_episodes=1000, n_win_ticks=195, gamma=0.995, display_when_done=True):
+    """
+    Q-Learning algorithm for the CartPole Environment. Finds the optimal greedy 
+    policy while following an epsilon-greedy policy.
 
-Args:
-    env: CartPole-v1 OpenAI gym environment.
-        nActions = 2
-    num_episodes: Number of episodes to sample.
-    discount_factor: Gamma discount factor.
-    epsilon: Chance of sampling a random action.
-    alpha: TD learning rate.
+    Args:
+        buckets:
+        n_episodes: Number of episodes to sample.
+        n_win_ticks: Average return necessery to consider the problem solved.
+        gamma: Gamma discount factor.
+        display_when_done: Render env after completing solving requirements.
 
-Returns:
-    Q: The optimal action-value function. A dictionary that maps from state -> value.
-"""
-def QCartPoleSolver(buckets=(1, 1, 6, 12,), n_episodes=1000, n_win_ticks=195, min_alpha=0.1, min_epsilon=0.1, gamma=1.0, ada_divisor=25):
+    Returns:
+        Q: The optimal action-value function. 
+    """
+    epsilon = 1.0 # probability of choosing a random action
+    epsilon_decay = 0.98 # decay of epsilon per episode
+    min_epsilon = 0
 
+    alpha = 1
+    alpha_decay = 0.98
+    min_alpha = 0.1
+
+    # CartPole-v1 OpenAI gym environment.
     env = gym.make('CartPole-v1')
 
+    # Action-value function
     Q = np.zeros(buckets + (env.action_space.n,))
 
     def discretize(obs):
@@ -56,26 +64,20 @@ def QCartPoleSolver(buckets=(1, 1, 6, 12,), n_episodes=1000, n_win_ticks=195, mi
     def choose_action(state, epsilon):
         return env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(Q[state])
 
-    def get_epsilon(t):
-        return max(min_epsilon, min(1, 1.0 - math.log10((t + 1) / ada_divisor)))
-
-    def get_alpha(t):
-        return max(min_alpha, min(1.0, 1.0 - math.log10((t + 1) / ada_divisor)))
 
     scores = deque(maxlen=100)
     learned = False
-    test = 0
+    if display_when_done: display = 0
 
     for e in range(n_episodes):
+        
         current_state = discretize(env.reset())
-
-        alpha = get_alpha(e)
-        epsilon = get_epsilon(e)
         i = 0
             
         while True:
-            if test >= 1:
-                env.render()
+            if display_when_done:
+                if display >= 1:
+                    env.render()
             action = choose_action(current_state, epsilon)
             obs, reward, done, _ = env.step(action)
             new_state = discretize(obs)
@@ -87,23 +89,43 @@ def QCartPoleSolver(buckets=(1, 1, 6, 12,), n_episodes=1000, n_win_ticks=195, mi
             current_state = new_state
             i += 1
 
+        
+        # decay exploration probability
+        # Decaying epsilon over time allow us to explore random actions in the
+        # beginning, but overtime we start to act more greedy and exploit the
+        # actions that we aready know will give us the most reward for that 
+        # particular state.
+        epsilon *= epsilon_decay
+        epsilon = max(epsilon, min_epsilon)
+
+        # learning rate decay
+        # At the beginning, you want higher alpha so that you can take big steps
+        # and learn things but while the agent is learning you should decay alpha
+        # to stabilize the model output and eventually converge to an optimal policy.
+        alpha *= alpha_decay
+        alpha = max(alpha, min_alpha)
 
         scores.append(i)
         mean_score = np.mean(scores)
         if mean_score >= n_win_ticks and e >= 100:
             learned = True 
-            print('Ran {} episodes. Solved after {} trials ✔'.format(e, e - 100))
-            if test >= 1: 
+            print(f'Ran {e} episodes. Solved after {e-100} trials ✔')
+            if display_when_done:
+                if display >= 1: 
+                    break
+                # Run one more time but tis time with display enabled
+                display += 1
+            else:
                 break
-            test += 1
         if e % 100 == 0:
-            print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e, mean_score))
+            print(f'[Episode {e}] - Mean survival time over last 100 episodes was {mean_score} ticks.')
 
     if not learned:
-        print('Did not solve after {} episodes 😞'.format(e))
+        print(f'Did not solve after {e} episodes 😞')
 
     env.close()
+    return Q
 
 if __name__ == "__main__":
     
-    QCartPoleSolver()
+    Q = QCartPoleSolver()
